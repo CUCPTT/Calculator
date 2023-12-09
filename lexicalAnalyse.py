2fb877e1fbe34c138c843d4c8adbab1afe81302b
shihui <shihui_0723@126.com>
2023-12-09 21:58:48 +0800

    增加非法字符处理，更改输出格式

--- lexicalAnalyse.py
+++ lexicalAnalyse.py
@@ -1,47 +1,69 @@
 import re
 
-# 定义正则表达式模式
-patterns = [
-    ('NUMBER', r'-?\d+(\.\d+)?'),  # 匹配整数、小数、负数
-    ('ARITHMETIC_OP', r'\+|-|\*|/|%|\*\*|//'),  # 算术运算符
-    ('LOGICAL_OP', r'&|\||~|\^|<<|>>'),  # 逻辑运算符
-    ('FUNCTION', r'sin\(|cos\(|tan\(|ln\(|sqrt\('),  # 函数
-    ('OTHER', r'\(|\)|\.|\s'),  # 其他标识符：括号、点、空格
-]
-
-# 构建正则表达式模式
-regex_patterns = '|'.join('(?P<%s>%s)' % pair for pair in patterns)
-
-# 进行词法分析
 def lexer(input_string):
+
+    input_string = input_string.replace(' ','')
+
+    legal_chars = ['0','1','2','3','4','5','6','7','8','9','.','(',')','+','-','*','/','%','&','|','^','~','<','>','s','i','n','c','o','t','a','l','q','r']
+    illegal_chars = []
+    for char in input_string:
+        if char not in legal_chars:
+            illegal_chars.append(char)
+
+    if illegal_chars:
+        print("Error: Invalid character(s): {}".format(illegal_chars))
+        return False
+    
     tokens = []
-    for match in re.finditer(regex_patterns, input_string):
-        for name, value in match.groupdict().items():
-            if value is not None and not (name == 'OTHER' and value == ' '):
-                tokens.append((name, value))
-                break  # 找到匹配的标记后立即停止
 
-    # 将连续的 '*' 组合为 '**'
-    combined_tokens = []
-    i = 0
-    while i < len(tokens):
-        if tokens[i][1] == '*' and i + 1 < len(tokens) and tokens[i + 1][1] == '*':
-            combined_tokens.append(('ARITHMETIC_OP', '**'))
-            i += 2
-        else:
-            combined_tokens.append(tokens[i])
-            i += 1
+    operators = {'+': 'arithmetic', '-': 'arithmetic', '*': 'arithmetic', '/': 'arithmetic', '%': 'arithmetic', '**': 'arithmetic', '//': 'arithmetic',
+                 '&': 'logical', '|': 'logical', '~': 'logical', '^': 'logical', '<<': 'logical', '>>': 'logical'}
+        
+    # Regular expressions for matching patterns
+    number_pattern = r'(\d+(\.\d+)?)'
+    operator_pattern = r'(\*\*|//|<<|>>|[+\-*/%&|^~])'
+    function_pattern = r'(sin\(|cos\(|tan\(|ln\(|sqrt\()'
+    left_paren_pattern = r'(\()'
+    right_paren_pattern = r'(\))'
+    space_pattern = r'\s+'
 
-    # 输出标记序列（忽略空格）
-    output_tokens = [token[1] for token in combined_tokens if token[0] != 'OTHER' or token[1] != ' ']
+    # Combined regular expression pattern
+    combined_pattern = re.compile('|'.join([number_pattern, operator_pattern, function_pattern, left_paren_pattern, right_paren_pattern, space_pattern]))
+    matches = combined_pattern.findall(input_string)
+    
+    for match in matches:
 
-    return output_tokens
+        token_type = ''
+
+        if match[0]:  # Matched a number
+            if '.' in match[0]:
+                token_type = 'float'
+            elif match[0].isdigit():
+                token_type = 'integer'
+            else:
+                token_type = 'unsigned'
+            tokens.append(('number',token_type,match[0]))
+
+        elif match[2]:  # Matched an operator
+            token_type = operators.get(match[2])
+            tokens.append(('operator',token_type,match[2]))            
+
+        elif match[3]:  # Matched a function
+            token_type = 'function'
+            tokens.append(('left',token_type,match[3]))  
+
+        elif match[4]:  # Matched a left parenthesis
+            token_type = ''
+            tokens.append(('left',token_type,match[4]))  
+
+        elif match[5]:  # Matched a right parenthesis
+            token_type = ''
+            tokens.append(('right',token_type,match[5]))  
+
+    print(tokens)
+    
+    return tokens
 
 if __name__ == '__main__':
-
-    # 示例输入
-    input_string = "sin((1<<2) + 3**4)"
-
-    # 执行词法分析
-    tokens = lexer(input_string)
-    print(tokens)
+    input_str = 'sin((1<<2) + 3**4)'
+    result = lexer(input_str)
